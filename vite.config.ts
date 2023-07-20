e4eb7204b88f7c45b34e2ccad82fe85a0aa16929
Serhii Shabala <sergei181989@gmail.com>
2023-07-20 17:44:37 +0300

    :seedling: Init library project

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      data: '/src/data',
+      pages: '/src/pages',
+      utils: '/src/utils',
+      assets: '/src/assets',
+      components: '/src/components',
+      styles: path.resolve(__dirname, './src/styles/styles.scss'),
+    },
+  },
+});
